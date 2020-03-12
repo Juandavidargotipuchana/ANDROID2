67c9756aca0bd152ce067965bb414d7d2ff67985
juanargoti <juanargoti2015@hotmail.com>
2020-03-11 21:33:38 -0500

    Nuevos cambios

--- Market/app/src/main/java/com/example/market/LoginActivity.java
+++ Market/app/src/main/java/com/example/market/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.market;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +14,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+ public void btnlogin(View view){
+     //Toast.makeText(this, "your have presed sup Button ", Toast.LENGTH_SHORT).show();
+     Intent actHome = new Intent(this, HomeActivity.class);
+     startActivity(actHome);
+ }
+    public void btnsignup(View view){
+
+        //Toast.makeText(this, "your have presed sign up Button ", Toast.LENGTH_SHORT).show();
+    }
 }
+
+
+
+    
